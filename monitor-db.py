c470182da63ff2581f8c0c4e3794459398d9913a
seedubjay <cwj@dhcp-10-249-79-60.eduroam.wireless.private.cam.ac.uk>
2020-04-11 20:32:24 +0100

    add tqdm

--- monitor-db.py
+++ monitor-db.py
@@ -1,23 +1,23 @@
 from pymongo import MongoClient
 import time
+from tqdm import tqdm 
 
 DUMP_LANG = 'pt'
 DUMP_DATE = '20200401'
 
 db_client = MongoClient('localhost', 27017)
 db = db_client[f"wikipedia-{DUMP_LANG}wiki-{DUMP_DATE}"]
+page_db = db.pages
 file_db = db.uploaded_files
 
 def page_count():
     return list(file_db.aggregate([{'$group': {'_id': None, 'total': {'$sum': '$page_count'}}}]))[0]['total']
 
-start_time = time.time()
-start_count = page_count()
+count = page_count()
 
-print()
-print()
-while True:
-    time.sleep(.2)
-    print("\033[F\033[F", end='')
-    print(f"{round((page_count()-start_count)/(time.time()-start_time))} pages/s")
-    print(f"{page_count()} pages")
+with tqdm(initial=count, total=page_db.estimated_document_count()) as pbar:
+    while True:
+        time.sleep(.2)
+        prev = count
+        count = page_count()
+        pbar.update(count-prev)
