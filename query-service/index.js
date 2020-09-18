b0af56ec2a8f99eb674c15b8a2479330cfd350d6
seedubjay <cwj@dhcp-10-249-77-48.eduroam.wireless.private.cam.ac.uk>
2020-09-18 03:34:24 +0100

    add cors

--- query-service/index.js
+++ query-service/index.js
@@ -1,14 +1,26 @@
 let express = require("express");
+let cors = require("cors");
 let app = express();
 
 let port = process.env.PORT || 3000;
 
-app.get("/api/wikipedia-route/:source/:target", (req, res) => {
+let pages = []
+for (var i=0; i < 1000; i++) {
+    pages.push(Math.random().toString(36).substring(7));
+}
+
+app.use(cors());
+
+app.get("/wikipedia-route/pages", (req,res) => {
+    res.json(pages);
+})
+
+app.get("/wikipedia-route/:source/:target", (req, res) => {
     res.json([req.params.source, req.params.target])
     
 });
 
 app.listen(port, () => {
-    console.log("Server running on port 3000");
+    console.log(`Server running on port ${port}`);
    });
    
