905e1a2b6173441d238eff8752463d8df229428f
seedubjay <cwj@dhcp-10-249-77-48.eduroam.wireless.private.cam.ac.uk>
2020-09-18 01:11:08 +0100

    port

--- query-service/index.js
+++ query-service/index.js
@@ -1,12 +1,14 @@
-var express = require("express");
-var app = express();
+let express = require("express");
+let app = express();
+
+let port = process.env.PORT || 3000;
 
 app.get("/api/wikipedia-route/:source/:target", (req, res) => {
     res.json([req.params.source, req.params.target])
     
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server running on port 3000");
    });
    
