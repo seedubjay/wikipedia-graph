15682a0eff75ee515936a2d9e3562860de6128dd
seedubjay <cwj@dhcp-10-249-77-48.eduroam.wireless.private.cam.ac.uk>
2020-09-18 13:51:56 +0100

    bfs

--- query-service/index.js
+++ query-service/index.js
@@ -14,6 +14,33 @@ for (x of data.nodes) {
 for (x of data.edges) edges[x.source].push(x.target);
 console.info('Loaded graph')
 
+function path(source, target) {
+    let dist = new Map()
+    let prev = new Map()
+    dist.set(source, 0);
+    let active = [source];
+    let activeStart = 0;
+    while (activeStart < active.length && !dist.has(target)) {
+        let i = active[activeStart++];
+        for (j of edges[i]) {
+            if (!dist.has(j)) {
+                dist.set(j, dist.get(i) + 1);
+                prev.set(j, i);
+                active.push(j);
+            }
+        }
+    }
+
+    let soln = [target];
+    while (soln[soln.length-1] != source) {
+        soln.push(prev.get(soln[soln.length-1]));
+    }
+
+    return soln.map(i => labels[i]).reverse();
+}
+
+console.log(path(32771, 12))
+
 let app = express();
 app.use(cors());
 
