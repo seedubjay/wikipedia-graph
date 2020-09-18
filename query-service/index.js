5c6184491d5a0aa656f92396ba9077a7b9e6d1f4
seedubjay <cwj@dhcp-10-249-77-48.eduroam.wireless.private.cam.ac.uk>
2020-09-18 16:52:55 +0100

    iterative deepening

--- query-service/index.js
+++ query-service/index.js
@@ -5,14 +5,35 @@ const cors = require("cors");
 let port = process.env.PORT || 3000;
 
 let data = JSON.parse(fs.readFileSync('./graph.json', {encoding:'utf8', flag:'r'}));
-let labels = {}
-let edges = {}
+let labels = new Map()
+let edges = new Map()
 for (x of data.nodes) {
-    labels[x.id] = x.label;
-    edges[x.id] = [];
+    labels.set(x.id, x.label);
+    edges.set(x.id, []);
 }
-for (x of data.edges) edges[x.source].push(x.target);
+for (x of data.edges) edges.get(x.source).push(x.target);
 console.info('Loaded graph')
+
+function dfs(node, depth, path, target) {
+    path.push(node)
+    if (node === target) return path;
+    if (depth) {
+        for (e of edges.get(node)) {
+            let a = dfs(e, depth-1, path, target);
+            if (a) return a;
+        }
+    }
+    path.pop(node);
+    return null;
+}
+
+function deepening_path(source, target) {
+    for (let i = 0; i < 50; i++) {
+        let p = dfs(source, i, [], target);
+        if (p) return p.map(i => labels.get(i));
+    }
+    return null;
+}
 
 function path(source, target) {
     let dist = new Map()
@@ -22,7 +43,7 @@ function path(source, target) {
     let activeStart = 0;
     while (activeStart < active.length && !dist.has(target)) {
         let i = active[activeStart++];
-        for (j of edges[i]) {
+        for (j of edges.get(i)) {
             if (!dist.has(j)) {
                 dist.set(j, dist.get(i) + 1);
                 prev.set(j, i);
@@ -36,7 +57,7 @@ function path(source, target) {
         soln.push(prev.get(soln[soln.length-1]));
     }
 
-    return soln.map(i => labels[i]).reverse();
+    return soln.map(i => labels.get(i)).reverse();
 }
 
 let app = express();
@@ -49,7 +70,7 @@ app.get("/wikipedia-route/pages", (req,res) => {
 app.get("/wikipedia-route/:source/:target", (req, res) => {
     let {source, target} = req.params;
     console.info(`route query: ${source} ${target}`)
-    res.json(path(source,target))
+    res.json(deepening_path(source,target))
 });
 
 app.listen(port, () => {
