d264b50cdb5b5eecabfcf805b4bdb917aa26114d
seedubjay <cwj@CWJ-Macbook-Pro.lan>
2020-12-03 18:56:16 +0000

    move db out to file

---
+++ generate/db.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+import os
+
+DUMP_LANG = 'en'
+if 'WIKI_LANG' in os.environ: DUMP_LANG = os.environ['WIKI_LANG']
+DUMP_DATE = '20200901'
+if 'WIKI_DATE' in os.environ: DUMP_DATE = os.environ['WIKI_DATE']
+
+db_client = MongoClient('localhost', 27017)
+db = db_client[f"wikipedia-{DUMP_LANG}wiki-{DUMP_DATE}"]
