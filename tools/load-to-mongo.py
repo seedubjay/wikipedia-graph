45b59a73e4bcb38f64cfe24e3529fe63cf50591c
seedubjay <cwj@CWJ-Macbook-Pro.lan>
2021-02-09 00:32:57 +0000

    cleanup

---
+++ tools/load-to-mongo.py
@@ -0,0 +1,16 @@
+import sys
+from pymongo import MongoClient
+import pickle
+import tqdm
+
+db_client = MongoClient('localhost', 27017)
+db = db_client["wikipedia"]
+
+edges = pickle.load(open(sys.argv[1], "rb"))
+batch = []
+for s in tqdm(edges):
+    batch.append({'_id': s, 'links': edges[s]})
+    if len(batch) >= 1000:
+        db.graph.insert_many(batch)
+        batch = [] 
+db.graph.insert_many(batch)
